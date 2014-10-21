185111baea757308c5074ff2ca789edc658f4907
neomatrix369 <sadhak001@gmail.com>
2014-10-21 23:34:31 +0100

    Refactoring Specifications - 07 Writing more tests (happy path)

---
+++ java/07UnitTestsOneHappyPath/src/test/java/unit_tests/WordFrequencyReporterBehaviours.java
@@ -0,0 +1,87 @@
+package unit_tests;
+
+import com.neomatrix369.WordsFrequencyReporter;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class WordFrequencyReporterBehaviours {
+    private static final String SHOULD_HAVE_RETURNED_AN_EMPTY_LIST = "Should have returned an empty list";
+    private static final String EMPTY_WORDS_LIST = "";
+    private static final String NOT_ASSIGNED = null;
+    private static final int ANY_VALID_TOP_N = 1;
+    private StringBuilder expectedList;
+
+    /*
+        Scenario: Frequency report for an undefined list (null)
+            Given an empty list is available
+            When the frequency program is executed
+            Then an empty report is returned
+    */
+    @Test
+    public void shouldReturnAnEmptyReportWhenANullWordList_With_AnyValueForTopN_IsPassedIn() {
+        // Given
+        String expectedListAsString = getWordFrequencyEmptyReport();
+
+        // When
+        String actualListAsString = getWordsFrequencyReportFrom(ANY_VALID_TOP_N, NOT_ASSIGNED);
+
+        //Then
+        assertThat(SHOULD_HAVE_RETURNED_AN_EMPTY_LIST,
+                actualListAsString, is(equalTo(expectedListAsString)));
+    }
+
+    /*
+        Scenario: Frequency report for an empty list
+            Given an empty list is available
+            When the frequency program is executed
+            Then an empty report is returned
+    */
+    @Test
+    public void shouldReturnAnEmptyReportWhenAnEmptyList_With_AnyValueForTopN_IsPassedIn() {
+        // Given
+        String expectedListAsString = getWordFrequencyEmptyReport();
+
+        // When
+        String actualListAsString =
+                getWordsFrequencyReportFrom(ANY_VALID_TOP_N, EMPTY_WORDS_LIST);
+
+        //Then
+        assertThat(SHOULD_HAVE_RETURNED_AN_EMPTY_LIST,
+                actualListAsString, is(equalTo(expectedListAsString)));
+    }
+
+
+    /*
+        Scenario: Frequency report for a list with one word
+            Given a list with one word is available
+            When the frequency program is executed
+            Then an a report containing one word (the same) is returned
+    */
+    @Test
+    public void shouldReturnReportWithOneWordWhenAListWithASingleWord_With_AnyValueForTopN_IsPassedIn() {
+        // Given
+        String expectedListAsString = "1 united\n";
+        String wordList = "united";
+
+        // When
+        String actualListAsString = getWordsFrequencyReportFrom(ANY_VALID_TOP_N, wordList);
+
+        //Then
+        assertThat("Should have returned a list with one word.",
+                actualListAsString, is(equalTo(expectedListAsString)));
+    }
+
+    public String getWordFrequencyEmptyReport() {
+        expectedList = new StringBuilder();
+        return expectedList.toString();
+    }
+
+    private String getWordsFrequencyReportFrom(int topN, String wordsList) {
+        WordsFrequencyReporter wordsFrequencyBuilder = new WordsFrequencyReporter();
+        StringBuilder actualList = wordsFrequencyBuilder.parse(topN, wordsList);
+        return actualList.toString();
+    }
+}
