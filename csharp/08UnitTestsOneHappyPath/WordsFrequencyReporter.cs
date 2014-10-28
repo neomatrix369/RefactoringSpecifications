09524db41814316a0b5e3db59fd13094153fff99
Samir Talwar <samir@noodlesandwich.com>
2014-10-28 20:56:47 +0000

    C# 08: Unit tests, one happy path.

---
+++ csharp/08UnitTestsOneHappyPath/WordsFrequencyReporter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Text;
+
+namespace UnitTestsOneHappyPath
+{
+	public class WordsFrequencyReporter
+	{
+		public StringBuilder Parse(int topNumberOfWords, String wordsList)
+		{
+			return null;
+		}
+	}
+}
