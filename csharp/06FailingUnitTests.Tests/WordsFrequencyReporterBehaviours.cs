a87c626372e58b7b7a2a4dbf237cf722f03f0e60
Samir Talwar <samir@noodlesandwich.com>
2014-10-28 20:30:05 +0000

    C# 06: Failing unit tests.

---
+++ csharp/06FailingUnitTests.Tests/WordsFrequencyReporterBehaviours.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace FailingUnitTests.Tests
+{
+	[TestFixture()]
+	public class WordsFrequencyReporterBehaviours
+	{
+		/*
+	        Scenario: Frequency report for an empty list
+	            Given an empty list is available
+	            When the frequency program is executed
+	            Then an empty report is returned
+	    */
+		[Test()]
+		public void ShouldReturnAnEmptyReportWhenAnEmptyListAndForAnyValidTopNIsPassedIn()
+		{
+			// Given
+			string expectedList = "";
+			string wordsList = "";
+			int topN = 3;
+
+			// When
+			WordsFrequencyReporter wordsFrequencyBuilder = new WordsFrequencyReporter();
+			string actualList = wordsFrequencyBuilder.Parse(topN, wordsList).ToString();
+
+			//Then
+			Assert.AreEqual(expectedList, actualList);
+		}
+	}
+}
---
+++ csharp/06FailingUnitTests/WordsFrequencyReporter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Text;
+
+namespace FailingUnitTests
+{
+	public class WordsFrequencyReporter
+	{
+		public StringBuilder Parse(int topNumberOfWords, String wordsList)
+		{
+			return null;
+		}
+	}
+}
