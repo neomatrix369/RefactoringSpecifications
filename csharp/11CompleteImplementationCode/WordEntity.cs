fc6092736afca7a87ff37d3e5e28a104510833d4
Samir Talwar <samir@noodlesandwich.com>
2014-10-28 22:21:28 +0000

    C# 11: Complete implementation code.

---
+++ csharp/11CompleteImplementationCode/WordEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CompleteImplementationCode
+{
+	public class WordEntity
+	{
+		public string Word { get; set; }
+
+		public int Frequency { get; set; }
+
+		public void IncrementFrequency() {
+			Frequency++;
+		}
+
+		public bool isTheSameAs(String word) {
+			return Word == word;
+		}
+	}
+}
---
+++ csharp/11CompleteImplementationCode/WordsFrequencyReporter.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CompleteImplementationCode
+{
+	public class WordsFrequencyReporter
+	{
+		private const string EACH_LINE_REPORT_FORMAT = "{0} {1}\n";
+
+		public StringBuilder Parse(int topNFrequency, String wordsList) {
+			StringBuilder report = new StringBuilder();
+
+			if (CheckIfWordsListIsNotAssignedOrEmpty(wordsList))
+			{
+				return report;
+			}
+
+			WordsFrequencyList wordsFrequencyHelper = new WordsFrequencyList();
+			var wordsFrequencyList = wordsFrequencyHelper.CalculateWordsFrequency(wordsList)
+				.OrderBy(word => word, new SortByFrequencyDescAndByWordAlphabetically())
+				.ToList();
+
+			return GenerateReportToReturn(report, wordsFrequencyList, topNFrequency);
+		}
+
+		private StringBuilder GenerateReportToReturn(StringBuilder report, ICollection<WordEntity> wordsFrequencyList, int topNFrequency)
+		{
+			if (topNFrequency <= 0)
+			{
+				return report;
+			}
+
+			topNFrequency = KeepTopNFrequencyWithinRange(wordsFrequencyList, topNFrequency);
+
+			var filteredList = wordsFrequencyList.Take(topNFrequency);
+			foreach (WordEntity wordEntity in filteredList)
+			{
+				report.AppendFormat(EACH_LINE_REPORT_FORMAT, wordEntity.Frequency, wordEntity.Word);
+			}
+
+			return report;
+		}
+
+		private int KeepTopNFrequencyWithinRange(ICollection<WordEntity> wordsFrequencyList, int topNFrequency)
+		{
+			return Math.Min(topNFrequency, wordsFrequencyList.Count);
+		}
+
+		private bool CheckIfWordsListIsNotAssignedOrEmpty(String wordsList) 
+		{
+			return WordsListIsNotAssigned(wordsList) || wordsList.Length == 0;
+		}
+
+		private bool WordsListIsNotAssigned(String wordsList) 
+		{
+			return wordsList == null;
+		}
+	}
+}
