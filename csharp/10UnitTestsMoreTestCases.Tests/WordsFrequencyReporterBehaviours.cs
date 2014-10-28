cabd89056081cbcae2b60604ea3dbef647e94c5c
Samir Talwar <samir@noodlesandwich.com>
2014-10-28 21:44:15 +0000

    C# 10: Unit tests, more test cases.

---
+++ csharp/10UnitTestsMoreTestCases.Tests/WordsFrequencyReporterBehaviours.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using System;
+using System.Text;
+
+namespace UnitTestsMoreTestCases.Tests
+{
+	[TestFixture()]
+	public class WordsFrequencyReporterBehaviours
+	{
+		private const string EMPTY_REPORT = "";
+
+		//        word list          expected count             expected result
+		[TestCase(null,              1,                Result = EMPTY_REPORT)]
+		[TestCase("",                2,                Result = EMPTY_REPORT)]
+		[TestCase("united",          1,                Result = "1 united\n")]
+		[TestCase(" united",         1,                Result = "1 united\n")]
+		[TestCase(" united ",        1,                Result = "1 united\n")]
+		[TestCase(" united, ",       1,                Result = "1 united\n")]
+		[TestCase(" united, ,",      1,                Result = "1 united\n")]
+		[TestCase("united, kingdom", 2,                Result = "1 kingdom\n"
+		                                                      + "1 united\n")]
+		[TestCase("united, "
+		        + "kingdom, "
+		        + "russia",          2,                Result = "1 kingdom\n"
+		                                                      + "1 russia\n")]
+		[TestCase("united, kingdom, "
+		        + "united, states",  3,                Result = "2 united\n"
+		                                                      + "1 kingdom\n"
+		                                                      + "1 states\n")]
+		public string ShouldReturnReportWhen_AWordList_With_AValueForTopN_IsPassedIn(string wordsList, int topNFrequency)
+		{
+			// Given
+			// Refer to table above for all combination and permutation
+			// of topNFrequency and wordsList
+
+			// When
+			WordsFrequencyReporter wordsFrequencyBuilder = new WordsFrequencyReporter();
+			StringBuilder actualList = wordsFrequencyBuilder.Parse(topNFrequency, wordsList);
+
+			// Then
+			return actualList.ToString();
+		}
+	}
+}
---
+++ csharp/10UnitTestsMoreTestCases/WordsFrequencyReporter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Text;
+
+namespace UnitTestsMoreTestCases
+{
+	public class WordsFrequencyReporter
+	{
+		public StringBuilder Parse(int topNumberOfWords, String wordsList)
+		{
+			return null;
+		}
+	}
+}
